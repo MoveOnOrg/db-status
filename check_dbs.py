8f9aed8eb4065daa84942931c3223bd91ea8a363
Sandra Chung <sandra@moveon.org>
2017-09-01 16:02:29 -0600

    ready for primetime

---
+++ check_dbs.py
@@ -0,0 +1,12 @@
+from connect import connect_psql, connect_mysql, post_to_slack
+import settings
+
+def main():
+    for key, database in settings.databases.items():
+        if database['type'] == 'mysql':
+            connect_mysql(database)
+        elif database['type'] == 'psql':
+            connect_psql(database)
+
+if __name__ == "__main__":
+    main()
--- connect.py
+++ connect.py
@@ -17,7 +17,7 @@ def connect_psql(psql):
     except:
         message = "{} connection error: {}".format(psql['name'],sys.exc_info())
         if settings.slack_alerts:
-            post_to_slack_channel(message)
+            post_to_slack(message)
 
 
 def connect_mysql(mysql):
@@ -32,10 +32,10 @@ def connect_mysql(mysql):
     except:
         message = "{} connection error: {}".format(mysql['name'],sys.exc_info())
         if settings.slack_alerts:
-            post_to_slack_channel(message)
+            post_to_slack(message)
 
 
-def post_to_slack_channel(message, slack_webhook_url=settings.slack_webhook_url):
+def post_to_slack(message, slack_webhook_url=settings.slack_webhook_url):
     text = json.dumps({ 'text' : message })
     print(text)
     r = requests.post(slack_webhook_url, data = text)
